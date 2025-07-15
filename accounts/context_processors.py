a6002f9dc7f52fde912324c633a277a29c34216b
ahmed <ahmedibrahim.199820@gmail.com>
2025-07-15 08:01:02 +0300

    first commit

---
+++ accounts/context_processors.py
@@ -0,0 +1,7 @@
+from .models import Notification  
+def notifications_processor(request):
+    if request.user.is_authenticated:
+        notifications = Notification.objects.filter(user=request.user, is_deleted=False).order_by('-created_at')
+    else:
+        notifications = []
+    return {'notifications': notifications}
